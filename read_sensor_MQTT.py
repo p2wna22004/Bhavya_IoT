70cfde538a5f5b57ef4664b253c37178084d2d5e
p2wna22004 <127719521+p2wna22004@users.noreply.github.com>
2023-04-23 21:59:11 +0530

    Add files via upload

---
+++ read_sensor_MQTT.py
@@ -0,0 +1,22 @@
+import paho.mqtt.client as mqtt
+import time import strftime
+
+from w1thermsensor import W1ThermSensor
+#Establish mqtt connection
+topic = "sensor_data"
+client = mqtt.Client()
+client.connect('34.125.211.55',1883,60)
+
+#Read and publish sensor information
+for sensor in W1ThermSensor.get_available_sensors():
+    ID = sensor.id
+    temperature = sensor.get_temperature()
+    Time = strftime("%d/%m/%y at %I:%M%p")
+    payload["ID"]=ID
+    payload["temperature"]=temperature
+    payload["Time"]=Time
+    (rc,mid)=client.publish(topic,payload);
+    
+    
+client.disconnect()
+
